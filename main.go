69b55466370960076a412253f2ffe27d8def35d8
Gabriel Falkenberg <gabriel@soundtrackyourbrand.com>
2015-04-29 12:47:07 +0200

    Vendor dependencies.

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/go-martini/martini"
+import "github.com/gabrielf/godep-sandbox/Godeps/_workspace/src/github.com/go-martini/martini"
 
 func main() {
 	m := martini.Classic()
